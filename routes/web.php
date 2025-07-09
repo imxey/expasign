7aee4d2bea74418012e5d79fcd38e4f20f512264
root <root@localhost.localdomain>
2025-07-09 16:31:13 +0000

    sitemap

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/sitemap.xml', function () {
+    $urls = [
+        secure_url('/'),
+        // Tambahin semua route kamu di sini
+    ];
+
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    $xml .= '<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">';
+
+    foreach ($urls as $url) {
+        $xml .= '<url>';
+        $xml .= '<loc>' . htmlentities($url) . '</loc>';
+        $xml .= '<lastmod>' . now()->toAtomString() . '</lastmod>';
+        $xml .= '<changefreq>weekly</changefreq>';
+        $xml .= '<priority>0.8</priority>';
+        $xml .= '</url>';
+    }
+
+    $xml .= '</urlset>';
+
+    return response($xml, 200)
+        ->header('Content-Type', 'application/xml');
+});
