3f584ba7d9f203dc0774616295403bf27beab752
Xeyla Vithra Arfhina <xeyla@xeyla.xeyla>
2025-07-12 00:02:17 +0700

    .

--- database/migrations/2025_07_11_134006_registrant.php
+++ database/migrations/2025_07_11_134006_registrant.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         //
         Schema::create('registrants', function (Blueprint $table) {
-            $table->uuid()->primary();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone');
