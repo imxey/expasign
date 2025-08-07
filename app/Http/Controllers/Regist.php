7177996f10e2bbfaa0bae10f4d99204cb17d758f
xeyla <xeylaxeyla@gmail.com>
2025-08-07 23:36:56 +0700

    edit wa msg

--- app/Http/Controllers/Regist.php
+++ app/Http/Controllers/Regist.php
@@ -86,11 +86,10 @@ class Regist extends Controller
             ]);
         } else {
             Http::withHeaders([
-                'Authorization' => env('FONNTE_TOKEN'),
-            ])->post('https://api.fonnte.com/send', [
-                'target' => env('FONNTE_NUMBER'),
+                'api_key' => env('API_KEY'),
+            ])->post(' https://api.expasign-edutime.site/send', [
+                'chatId' => env('WA_NUMBER'),
                 'message' => "*PENDAFTAR EXPASIGN BARU*\n\nNama: {$registrant->name}\nPhone: {$registrant->phone}\nEmail: {$registrant->email}\nNIM: {$registrant->nim}\nSekolah/Universitas: {$registrant->school}\nKategori: {$registrant->category}\nNominal: {$registrant->nominal}\n\nSilahkan buka https://expasign-edutime.site/admin dan verifikasi pembayaran",
-                'countryCode' => '62',
             ]);
             return response()->json(['success' => 'Pendaftaran berhasil!']);
         }
@@ -123,11 +122,10 @@ class Regist extends Controller
         }
 
         $response = Http::withHeaders([
-            'Authorization' => env('FONNTE_TOKEN'),
-        ])->post('https://api.fonnte.com/send', [
-            'target' => env('FONNTE_NUMBER'),
+            'api_key' => env('API_KEY'),
+        ])->post(' https://api.expasign-edutime.site/send', [
+            'chatId' => env('WA_NUMBER'),
             'message' => "*PENDAFTAR EXPASIGN BARU (OTOMATIS)*\n\nNama: {$db->name}\nPhone: {$db->phone}\nEmail: {$db->email}\nNIM: {$db->nim}\nSekolah/Universitas: {$db->school}\nKategori: {$db->category}\nNominal: {$db->nominal}\nstatus: {$db->status}\n",
-            'countryCode' => '62',
         ]);
 
         return response()->json(['message' => 'Callback berhasil diproses']);
